f235bc6f9b9bcb3f7eeabbce9256fde5230d2363
Sam Byron <mrc4090@proton.me>
2023-09-21 17:41:00 -0700

    Allow user to specify scribe name

--- activate.sh
+++ activate.sh
@@ -15,10 +15,11 @@ deactivate () {
 # unset irrelevant variables
 deactivate nondestructive
 
+read scribe_name
 if [ -z "${VIRTUAL_ENV_DISABLE_PROMPT:-}" ] ; then
     _OLD_VIRTUAL_PS1="${PS1:-}"
-    PS1="(scribe) ${PS1:-}"
+    PS1="($scribe_name) ${PS1:-}"
     export PS1
-    VIRTUAL_ENV_PROMPT="(scribe) "
+    VIRTUAL_ENV_PROMPT="($scribe_name) "
     export VIRTUAL_ENV_PROMPT
 fi
